e42424862819d81ae6de359bd7088205f46cbc2b
Rafi Fuady <rafifuady@gmail.com>
2022-02-05 09:18:19 +0700

    feat: init MUI8 minimal template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-function App() {
+/**
+ * MUI Imports
+ */
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+
+/**
+ * custom file Imports
+ */
+import theme from "./styles/theme";
+import createEmotionCache from "./common/utils/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+function App({ emotionCache = clientSideEmotionCache }) {
   return (
-    <>
-      POKEDEX
-    </>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        POKEDEX
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
